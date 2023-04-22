2007bb27a347119c4ce0f2d3a438968e05500e5f
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-04-22 12:10:20 +0800

    Idlis server

--- app/src/main/java/com/example/pc/doh/Urls.java
+++ app/src/main/java/com/example/pc/doh/Urls.java
@@ -4,8 +4,8 @@ public class Urls {
     // http://doholrs.infoadvance.com.ph/
     //public static String server = "https://doholrs.rightapps.ph/";
 //    public static String  server = "https://olrs.rightapps.solutions/";
-    public static String  server = "https://olrs.doh.gov.ph/";
-//    public static String  server = "https://idlis.infoadvance.com.ph/";
+//    public static String  server = "https://olrs.doh.gov.ph/";
+    public static String  server = "https://idlis.infoadvance.com.ph/";
     //public static String server = "http://192.168.254.104/doholrs/";
     //public static String employee = server + "employee/true";
     //public static String employee = server + "samplepadogs/true";
